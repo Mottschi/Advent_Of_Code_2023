e145a8c10d60cd6c72283fd98c920c8046b19def
Sebastian Mottschall <srophkor@gmail.com>
2023-12-03 16:23:13 +0100

    day 3 part 2

--- python/day03.py
+++ python/day03.py
@@ -73,13 +73,83 @@ def part_one(data):
 
 
 def part_two(data):
-	pass
+	ROW_LENGTH = len(data[0])
+	COL_LENGTH = len(data)
+	DIGITS = '0123456789'
+	GEAR_SYMBOL = '*'
+
+	result = 0
+
+	# rather then checking all numbers for gear symbol neighbors, we look for the gear symbols directly
+	# and then check how many numbers they have as neighbors
+
+	for y in range(COL_LENGTH):
+		if GEAR_SYMBOL not in data[y]:
+			continue
+		for x in range(ROW_LENGTH):
+			current_char = data[y][x]
+			if current_char == GEAR_SYMBOL:
+				found_neighbors_count = 0
+				found_neighbors_coords = []
+				# when we find a gear symbol, we need to check the neighbors to find numbers
+				# the two neighbors in the current line we can check directly for digits, each digit
+				# found there indicates a number
+				if x > 0 and data[y][x-1] in DIGITS:
+					found_neighbors_count += 1
+					found_neighbors_coords.append((y, x-1))
+
+				if x < ROW_LENGTH - 1 and data[y][x+1] in DIGITS:
+					found_neighbors_count += 1
+					found_neighbors_coords.append((y, x+1))
+
+				# for the lines above and below the current line, it is a bit more tricky
+				# if the middle character of the three neighbors in that line is a digit, then there is exactly one neighbor in that line
+				# otherwise, we need to check both corner characters, each digit found there adds one neighbor number
+				if y > 0:
+					if data[y-1][x] in DIGITS:
+						found_neighbors_count += 1
+						found_neighbors_coords.append((y-1, x))
+					else:
+						if x > 0 and data[y-1][x-1] in DIGITS:
+							found_neighbors_count += 1
+							found_neighbors_coords.append((y-1, x-1))
+
+						if x < ROW_LENGTH - 1 and data[y-1][x+1] in DIGITS:
+							found_neighbors_count += 1
+							found_neighbors_coords.append((y-1, x+1))
+				
+				if y < COL_LENGTH - 1:
+					if data[y+1][x] in DIGITS:
+						found_neighbors_count += 1
+						found_neighbors_coords.append((y+1, x))
+					else:
+						if x > 0 and data[y+1][x-1] in DIGITS:
+							found_neighbors_count += 1
+							found_neighbors_coords.append((y+1, x-1))
+
+						if x < ROW_LENGTH - 1 and data[y+1][x+1] in DIGITS:
+							found_neighbors_count += 1
+							found_neighbors_coords.append((y+1, x+1))
+
+				if found_neighbors_count == 2:
+					# parse both numbers, cast to int and multiply, then add result to result
+					numbers = []
+					for n_y, n_x in found_neighbors_coords:
+						while n_x > 0 and data[n_y][n_x-1] in DIGITS:
+							n_x -= 1
+						number = ''
+						while n_x < ROW_LENGTH and data[n_y][n_x] in DIGITS:
+							number += data[n_y][n_x]
+							n_x += 1
+						numbers.append(int(number))
+					result += numbers[0] * numbers[1]
+
+	return result	
 
 
 if __name__ == '__main__':
 	data = load()
 	data = parse(data)
-	print(data)
 
 	start_time = time.time()
 	print('Part One:', part_one(data))
