6293d7b50a7a9e52a115667cdd07467cd29d4b4f
Sebastian Mottschall <srophkor@gmail.com>
2023-12-04 06:30:36 +0100

    day 4 part 1

--- python/day04.py
+++ python/day04.py
@@ -1,6 +1,6 @@
 import time
 
-DEBUG = True
+DEBUG = 0
 
 if DEBUG:
 	FILE = '../inputs/day04_debug.txt'
@@ -15,11 +15,30 @@ def load():
 
 
 def parse(data):
-	return data
+	numbers = []
+	for line in data:
+		all_numbers = line.split(': ')[1]
+		seperated_numbers = all_numbers.split(' | ')
+		winning_numbers = [int(x) for x in seperated_numbers[0].split()]
+		card_numbers = [int(x) for x in seperated_numbers[1].split()]
+		numbers.append({'winning_numbers': winning_numbers, 'card_numbers': card_numbers})
+	return numbers
 
 
 def part_one(data):
-	pass
+	total_points = 0
+	for index, line in enumerate(data):
+		game = index + 1
+		game_points = 0
+		winning_numbers = 0
+		for winning_number in line['winning_numbers']:
+			if winning_number in line['card_numbers']:
+				winning_numbers += 1
+		if winning_numbers > 0:
+			game_points = 2 ** (winning_numbers - 1)
+		total_points += game_points
+	return total_points
+
 
 
 def part_two(data):
